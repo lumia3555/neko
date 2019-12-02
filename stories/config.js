6e9e2b930b43623908070356262cb01a420e0cce
zefeng <zefeng.zhou@shopee.com>
2019-12-02 17:03:41 +0800

    init

---
+++ stories/config.js
@@ -0,0 +1,9 @@
+import Introduce from './pages/base/introduce/index';
+
+export default [
+  {
+    title: '介绍',
+    module: '基本',
+    component: Introduce
+  }
+];
---
+++ stories/pages/base/introduce/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      react 组件介绍
+    </div>
+  );
+};
